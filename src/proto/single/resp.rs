d4971eebef6249840137db3cd16db70ba6ffa4d5
Hoang Dang <hoangd@turinganalytics.net>
2023-08-02 20:52:12 -0600

    println for read

--- src/proto/single/resp.rs
+++ src/proto/single/resp.rs
@@ -109,14 +109,15 @@ enum RawResponse {
 
 fn read<R: BufRead>(mut r: R) -> Result<RawResponse, Error> {
     let mut cmdbuf = [0u8; 1];
+    let now = std::time::Instant::now();
     r.read_exact(&mut cmdbuf)?;
+    println!("Read command in {} ms", now.elapsed().as_millis());
     match cmdbuf[0] {
         b'+' => {
-            // Simple String
-            // https://redis.io/topics/protocol#resp-simple-strings
+            let now = std::time::Instant::now();
             let mut s = String::new();
             r.read_line(&mut s)?;
-
+            println!("Read line in {} ms", now.elapsed().as_millis());
             // remove newlines
             let l = s.len() - 2;
             s.truncate(l);
